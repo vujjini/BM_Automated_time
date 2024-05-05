599d1c9ecf3af50a1e5d62bb4f6d28d505c408cf
Gamonal <henriquegamonal3@gmail.com>
2024-05-04 23:14:59 -0400

    Added class file and modified main file

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-
+from helper_classes import Events
+def main():
+    events = Events()
+    room = "2708"
+    event_name = "Random event"
+    print(event_name)
+    events.day[room] = {}
+    events.day[room][event_name] = [1, 2]
+    print(events.day[room][event_name][0])
+    return 0
+main()
